80f27109869b10f738bb01760873bb210352a3d2
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-21 14:33:03 +0100

    Added weather component to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React from "react";
 import Titles from "./components/Titles"
 //Imports Form object from Form.js
 import Form from "./components/Form"
+import Weather from "./components/Weather"
 
 //Instaniate React object by calling a class that extends React.Component
 class App extends React.Component {
@@ -12,8 +13,10 @@ class App extends React.Component {
   render(){
     return(
       <div>
+        //Components added within parent div element
         <Titles />
         <Form />
+        <Weather />
       </div>
     );
   }
