c022896d12f6e668fb5c969acd77d6a0ce336b8b
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-21 17:39:46 +0100

    Corrected API key

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import Titles from "./components/Titles"
 import Form from "./components/Form"
 import Weather from "./components/Weather"
 
-const API_KEY = process.env.REACT_APP_API_KEY
-
+const API_KEY = process.env.REACT_APP_API_KEY;
 //Instaniate React object by calling a class that extends React.Component
 class App extends React.Component {
   //Async is a way to make web requests easily
-  getWeather = async () => {
+  //Pass in e as 'event'
+  getWeather = async (e) => {
+    //Prevent page from refreshing
+    //Prevents default behaviour of component
+    e.preventDefault();
     //Fetch is a built in JS method to 'fetch' html
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/
-      weather?q=london&appid=${API_KEY}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=london&appid=${API_KEY}`);
     //We convert our api_call into json that we can manipulate
     const data = await api_call.json();
+    console.log(data);
   }
   //Returns JSX code
   render(){
     return(
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
