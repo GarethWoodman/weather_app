d2e0ef72786785f10f3ac1aef291baab75cbafda
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-23 16:22:03 +0100

    Added more CSS styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,12 +4,30 @@ import React from "react";
 //We can also omit the render() method
 const Weather = props => (
   // props only get displayed if they are NOT undefined
-  <div>
-    { props.city && props.country && <p>Location:    { props.city }, { props.country }</p> }
-    { props.temperature && <p>Temperature: { props.temperature }</p> }
-    { props.huidity && <p>Humidity: { props.humidity }</p> }
-    { props.description && <p>Conditions: { props.description }</p> }
-    { props.error && <p>{ props.error }</p> }
+  <div className="weather__info">
+    {
+      props.city && props.country && <p className="weather__key"> Location:
+      <span className="weather__value"> { props.city }, { props.country }</span>
+      </p>
+    }
+    {
+      props.temperature && <p className="weather__key">Temperature:
+      <span className="weather__value"> { props.temperature }</span>
+      </p>
+    }
+    {
+      props.huidity && <p className="weather__key">Humidity:
+      <span className="weather__value"> { props.humidity }</span>
+      </p>
+    }
+    {
+      props.description && <p className="weather__key">Conditions:
+      <span className="weather__value"> { props.description }</span>
+      </p>
+    }
+    {
+      props.error && <p className="weather__error">{ props.error }</p>
+    }
   </div>
 )
 
